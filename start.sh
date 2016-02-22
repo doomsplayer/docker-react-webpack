c743dc38d2d1b9f3ac61150e97de871460375862
FayeHuang <root@docker02.faye.local>
2016-02-22 20:41:50 +0800

    change container start up docker image

--- start.sh
+++ start.sh
@@ -9,13 +9,12 @@ read WEB_DEV_SERVER_PORT
 echo "Enter mock server url (ex:http://10.10.10.10:8080). If no, press Enter to skip."
 read MOCK_API_URL
 
-
-DOCKER_IMAGE=react-webpack
+DOCKER_IMAGE=fayehuang/react-webpackk
 
 docker run -d \
            -p $SSH_PORT:22 \
            -p $WEB_DEV_SERVER_PORT:$WEB_DEV_SERVER_PORT \
            -e PORT=$WEB_DEV_SERVER_PORT \
            -e MOCK_SERVER=$MOCK_API_URL \
-           -v $ASSETS_PATH:/usr/src/app/ \
+           -v $PROJECT_PATH:/usr/src/app/ \
            $DOCKER_IMAGE
